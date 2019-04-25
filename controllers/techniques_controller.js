43b059eacec0ccd9d2438c8daeae8f49e2b3711e
Deva <dkothand@gmail.com>
2019-04-25 17:59:05 -0400

    new.ejs creates new technique, create route redirects to index

--- controllers/techniques_controller.js
+++ controllers/techniques_controller.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const techniques = express.Router();
+const Technique = require('../models/technique.js');
 
+
+// INDEX
 techniques.get('/', (req, res) => {
     res.render('index.ejs');
 })
 
-
+// NEW
 techniques.get('/new', (req, res) => {
     res.render('new.ejs');
 });
 
+// SHOW
+// CREATE
+techniques.post('/', (req, res) => {
+    // console.log(req.body)
+    // res.send(req.body);
+    Technique.create(req.body, (err, createdTechnique) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(createdTechnique)
+        res.redirect('/techniques')
+    });
+});
+// EDIT
+// UPDATE
+// DELETE
 module.exports = techniques;
