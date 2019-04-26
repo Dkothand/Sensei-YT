5ec06bee334878bd37d17b7a2f2c6cb140cd07df
Deva <dkothand@gmail.com>
2019-04-26 16:31:12 -0400

    implemented logout button and delete route in sessions controller to remove current user from sessions

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -25,4 +25,12 @@ sessions.post('/', (req, res) => {
     })
 });
 
+
+sessions.delete('/', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
+
+
 module.exports = sessions;
