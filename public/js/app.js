31572c44bfe71e4df474b13b964de8cd82981ecd
Deva <dkothand@gmail.com>
2019-04-30 09:30:12 -0400

    ajax request gets youtube videos from search bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,29 @@
-console.log('Test');
-console.log($);
-// Youtube API key: AIzaSyBLMfcprLCQxeQTD-PAhnmQlI6yCMOnXzk
+const youtubeApiKey = "AIzaSyBLMfcprLCQxeQTD-PAhnmQlI6yCMOnXzk";
+
+
+const youtubeApiCall = () => {
+    // Get search form val
+    const userQuery = $('#search').val();
+
+    $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: youtubeApiKey,
+            part: 'snippet',
+            q: userQuery
+        },
+        dataType: 'json',
+        type: 'GET'
+    }).done((data) => {
+        console.log(data.items);
+    })
+};
+
+// Document ready function
+$(() => {
+    $('#search-bar').on('submit', (e) => {
+        e.preventDefault();
+        youtubeApiCall();
+    })
+});
+// End Document ready function
