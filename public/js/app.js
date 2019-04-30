4b21f287f04bbbf63253a4bb04ca90740f7bd013
Deva <dkothand@gmail.com>
2019-04-30 13:35:13 -0400

    jQuery renders youtube search results to new page with thumbnails and title of video

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,60 @@
 const youtubeApiKey = "AIzaSyBLMfcprLCQxeQTD-PAhnmQlI6yCMOnXzk";
 
+// Takes video object and builds html card with classes for css styling
+
+/*
+<div class="col s12 m6 l3">
+    <div class="card hoverable">
+        <div class="card-content">
+            <div class="card-img">
+                <img src="https://img.youtube.com/vi/<%= videoId %>/hqdefault.jpg" alt="Youtube video thumbnail">
+            </div>
+            <a href="/techniques/<%= move.id %>"><h4><%= move.title %></h4></a>
+        </div>
+    </div>
+</div>
+ */
+
+const createCard = (obj) => {
+    // Build nested divs
+    const $colDiv = $('<div>').attr('class', 'col s12 m6 l3');
+    const $cardDiv = $('<div>').attr('class', 'card hoverable');
+    const $contentDiv = $('<div>').attr('class', 'card-content');
+    const $imgDiv = $('<div>').attr('class', 'card-img');
+
+    $colDiv.append(
+        $cardDiv.append(
+            $contentDiv.append($imgDiv)
+        )
+    );
+
+    // Make image
+    const imageUrl = obj.snippet.thumbnails.high.url;
+    const $img = $('<img>').attr('src', imageUrl);
+
+    $img.appendTo($imgDiv);
+
+    // Make title
+    const objTitle = obj.snippet.title;
+    const $title = $('<p>').text(objTitle);
+
+    $title.appendTo($contentDiv);
+
+    // return outer div will all content appended
+    return $colDiv;
+}
+
+// Takes array of video objects from YouTube API call and renders to DOM
+const renderSearchResults = (arr) => {
+    arr.forEach(video => {
+        // Build card
+        const $card = createCard(video)
+        
+        // Append to DOM
+        $('.results').append($card);
+    });
+};
+
 
 const youtubeApiCall = () => {
     // Get search form val
@@ -9,6 +64,7 @@ const youtubeApiCall = () => {
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
             key: youtubeApiKey,
+            maxResults: 5,
             part: 'snippet',
             q: userQuery
         },
@@ -16,6 +72,9 @@ const youtubeApiCall = () => {
         type: 'GET'
     }).done((data) => {
         console.log(data.items);
+        renderSearchResults(data.items);
+    }, (error) => {
+        console.log(error);
     })
 };
 
