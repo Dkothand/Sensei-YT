6ad7e6b0dfd1f648a6114abba516da572ec8d355
Deva <dkothand@gmail.com>
2019-04-30 13:34:07 -0400

    changed schema to store video id instead of entire link, updated index.ejs to access id

--- models/technique.js
+++ models/technique.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const techniqueSchema = new Schema({
     title: String,
     notes: [{type: String}],
-    link: String
+    videoId: String
 });
 
 const Technique = mongoose.model('Technique', techniqueSchema);
